3bc9e06281d0a42589952e6d07b0bb5023118c6f
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2021-11-08 23:47:52 -0300

    Create desafio_5.js

---
+++ desafio_5.js
@@ -0,0 +1,21 @@
+String.prototype.toJadencase=function(){
+    let i=0
+    let ini=0
+    let string_final=''
+    while  (i<string.length){
+    if (i==0){
+        ini=0
+        string_final+=string.toUpperCase()[ini]
+    }else if(string[i]===' '){
+        ini=i+1
+        string_final+=' '
+        string_final+=string.toUpperCase()[ini]
+        i=i+1
+    }else{
+        string_final+=string[i]
+    }
+    i++
+    }
+    
+} ;
+
