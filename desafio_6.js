e6a2182f845ebb70c8ce7ca97063201d2d4e7283
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2021-11-23 09:52:41 -0300

    Update desafio_6.js

--- desafio_6.js
+++ desafio_6.js
@@ -3,9 +3,9 @@ function toJadenCase(string){
     let ini=0
     let string_final=''
     while  (i<string.length){
-    if (i==0){
-        ini=0
-        string_final+=string.toUpperCase()[ini]
+        if (i==0){
+            ini=0
+            string_final+=string.toUpperCase()[ini]
     }else if(string[i]===' '){
         ini=i+1
         string_final+=' '
