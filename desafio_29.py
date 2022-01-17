0054fd78a6249a7f1cb0e04bf57825971b7d8861
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2022-01-17 16:23:14 -0300

    Create desafio_29.py

---
+++ desafio_29.py
@@ -0,0 +1,16 @@
+def dirReduc(vet):
+    i=0
+    while i<(len(vet)-1):
+        if (vet[i]=="NORTH" and vet[i+1]=="SOUTH") or (vet[i]=="SOUTH" and vet[i+1]=="NORTH"):
+           vet.pop(i)
+           vet.pop(i)
+           i=0
+        elif (vet[i]=="WEST" and vet[i+1]=="EAST") or (vet[i]=="EAST" and vet[i+1]=="WEST"):
+           vet.pop(i)
+           vet.pop(i)
+           i=0
+        else:
+           i+=1
+    
+    
+    return vet 
