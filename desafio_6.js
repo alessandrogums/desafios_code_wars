95e36062cb72015345a44566a1a348b88a1458c1
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2022-02-03 10:11:11 -0300

    Update desafio_6.js

--- desafio_6.js
+++ desafio_6.js
@@ -1,21 +1,7 @@
-function toJadenCase(string){
-    let i=0
-    let ini=0
-    let string_final=''
-    while  (i<string.length){
-        if (i==0){
-            ini=0
-            string_final+=string.toUpperCase()[ini]
-    }else if(string[i]===' '){
-        ini=i+1
-        string_final+=' '
-        string_final+=string.toUpperCase()[ini]
-        i=i+1
-    }else{
-        string_final+=string[i]
-    }
-    i++
-    }
-    return string_final 
-} ;
+def to_jaden_case(string):
+    string=string.split()
+    for k in range(len(string)):
+        aux=string[k].capitalize()
+        string[k]=aux
+    return ' '.join(string)
 
