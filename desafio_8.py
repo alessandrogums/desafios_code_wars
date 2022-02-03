1f471a8f57ed294c88902b11ed626b91edbd7da5
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2022-02-03 09:56:50 -0300

    Update desafio_8.py

--- desafio_8.py
+++ desafio_8.py
@@ -18,3 +18,18 @@ def move_zeros(array):
         array.append(0)
     
     return array
+
+#outra forma de se fazer
+def move_zeros(lista):
+    for k in range(len(lista)):
+        if lista[k]==0:
+            lista.insert(len(lista),0)
+            lista.remove(0)
+    
+    return lista
+
+#mais outra forma 
+def move_zeros(array):
+    qte=array.count(0)
+    array=list(filter(lambda x:x!=0 , array))
+    return array + [0]*qte 
