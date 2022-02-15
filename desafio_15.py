b0f71e4059741d62abb7cca2f4f0f49459d7474e
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2022-02-15 18:05:49 -0300

    Update desafio_15.py

--- desafio_15.py
+++ desafio_15.py
@@ -1,14 +1,7 @@
- def find_missing(vetor):
-      dif=[]
-      pos=valor=0
-      for i in range(1,len(vetor)):
-          progressao=vetor[i]-vetor[i-1]
-          dif.append(progressao)
-      for c in range(len(dif)):
+def find_missing(vetor):
+	dif=vetor[len(vetor)-1] - vetor[0]
+	pa=dif/len(vetor)
+	for i in range(len(vetor)-1):
+		if vetor[i+1] - vetor[i] != pa: 
+			return vetor[i] + pa  
 
-          if dif.count(dif[c]) == 1:
-              pos=c
-
-          else:
-              valor=dif[c]
-      return (vetor[pos] + valor)
